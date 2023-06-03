6067dc884bff91a9112125ed61e1ab27e325fa88
yan-tingciou <angelaavril04@gmail.com>
2023-06-03 16:54:53 +0800

    新增環境23

--- js/all.js
+++ js/all.js
@@ -3,4 +3,10 @@ $(document).ready(function(){
     (event).preventDefault();
     $('body').toggleClass('menu-show');
   });
+  $('.heart').click(function(event){
+    $(this).toggleClass('full');
+  });
+  $('.burger-menu h5').click(function(event){
+    $(this).toggleClass('add');
+  });
 });
